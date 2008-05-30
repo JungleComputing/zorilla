af60c858a15791fe18cf3e11a797fd46ea788ed0
Niels Drost <n.drost@esciencecenter.nl>
2008-05-30 11:35:26 +0000

    added pause/resume interface to "zet" (no actual implementation yet)

--- node/src/ibis/zorilla/apps/Zet.java
+++ node/src/ibis/zorilla/apps/Zet.java
@@ -7,18 +7,24 @@ import java.util.Map;
 public final class Zet {
 
     private static void usage() {
-        System.out.println("Set attributes of jobs or a node. "
-                + "Defaults to setting node attributes" + "\n"
-                + "usage:  Zet [OPTION]... [ATTRIBUTE=NEW_VALUE]..."
-                + "\n-na,  --node_address IP:PORT  address of node"
-                + "\n-j JOB                        specify "
-                + "which job to set attributes of");
+        System.out
+                .println("Set attributes of jobs or a node. "
+                        + "Defaults to setting node attributes"
+                        + "\n"
+                        + "usage:  Zet [OPTION]... [ATTRIBUTE=NEW_VALUE]..."
+                        + "\n--node_address IP:PORT | -na   address of node"
+                        + "\n--job JOB | -j                 specify which job to set attributes of"
+                        + "\n--pause | -p                   set max workers to 0 (pause job execution)"
+                        + "\n--resume | -r                  set max workers back to previous value"
+                        + "\n                               (resume job execution)");
+
     }
 
     public static void main(String[] command) {
-
         String job = null;
         int attribute = -1;
+        boolean pause = false;
+        boolean resume = false;
 
         try {
 
@@ -29,9 +35,16 @@ public final class Zet {
                         || command[i].equals("--node_address")) {
                     i++;
                     nodeSocketAddress = command[i];
-                } else if (command[i].equals("-j")) {
+                } else if (command[i].equals("-j")
+                        || command[i].equals("--job")) {
                     i++;
                     job = command[i];
+                } else if (command[i].equals("-p")
+                        || command[i].equals("--pause")) {
+                    pause = true;
+                } else if (command[i].equals("-r")
+                        || command[i].equals("--resume")) {
+                    resume = true;
                 } else if (command[i].equals("--help")) {
                     usage();
                     return;
@@ -49,31 +62,37 @@ public final class Zet {
                 }
             }
 
-            if (attribute == -1) {
-                System.err.println("no new attributes specified");
-                System.exit(1);
-            }
-
             Map<String, String> attributes = new HashMap<String, String>();
 
-            for (int i = attribute; i < command.length; i++) {
-                String[] parts = command[i].split("=");
-                if (parts.length != 2) {
-                    System.err
-                            .println("attribute should be defined as ATTRIBUTE=VALUE (not "
-                                    + command[i] + ")");
-                    System.exit(1);
+            
+            if (attribute != -1) {
+                for (int i = attribute; i < command.length; i++) {
+                    String[] parts = command[i].split("=");
+                    if (parts.length != 2) {
+                        System.err
+                                .println("attribute should be defined as ATTRIBUTE=VALUE (not "
+                                        + command[i] + ")");
+                        System.exit(1);
+                    }
+                    attributes.put(parts[0], parts[1]);
                 }
-                attributes.put(parts[0], parts[1]);
             }
 
             ZoniConnection connection = new ZoniConnection(nodeSocketAddress,
                     null);
 
+            if (attribute != -1) {
             if (job == null) {
                 connection.setNodeAttributes(attributes);
             } else {
                 connection.setJobAttributes(job, attributes);
+            }
+            }
+            
+            if (pause) {
+                System.err.println("PAUSE! :(");
+            } else if (resume) {
+                System.err.println("RESUME! :)");
             }
 
             connection.close();
